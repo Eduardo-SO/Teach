5a381a2d5f7fd373fbcb06f2f4793eaa20bad5d7
Eduardo SO <eduardo.sool07@gmail.com>
2020-09-20 23:33:32 -0300

    Add ESLint, Prettier and EditorConfig

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,16 +1,20 @@
+import React from 'react'
 import Head from 'next/head'
 
-export default function Home() {
+ const Home:React.FC = () => {
   return (
     <div>
       <Head>
         <title>Homepage</title>
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-       <h1>Hello world!</h1>
+        <h1>Hello world!</h1>
       </main>
     </div>
   )
 }
+
+export default Home;
