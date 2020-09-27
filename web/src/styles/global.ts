b854fe96847fd06e2bee8c660e81adb1346ff8aa
Eduardo SO <eduardo.sool07@gmail.com>
2020-09-27 04:09:09 -0300

    Add transition duration to theme switch

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -10,6 +10,9 @@ export default createGlobalStyle`
 
   body {
     background-color: ${props => props.theme.colors.background.primary};
+
+    transition: color 0.2s;
+    transition: background 0.2s;
   }
 
   body,
@@ -20,6 +23,11 @@ export default createGlobalStyle`
 
     -webkit-font-smoothing: antialiased;
     font: 400 16px 'Poppins';
+  }
+
+  input {
+    transition: color 0.2s;
+    transition: background 0.2s;
   }
 
   #__next, body, main {
