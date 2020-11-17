a6790188882b049624f4fb7fa84207a4b0dba98f
Eduardo SO <eduardo.sool07@gmail.com>
2020-11-17 00:28:29 -0300

    Add uuid

--- server/src/routes/students.ts
+++ server/src/routes/students.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express'
+import { v4 as uuid } from 'uuid'
 
 const studentsRouter = Router()
 
 studentsRouter.post('/', (request, response) => {
   const { name, email } = request.body
 
-  return response.json({ name, email })
+  return response.json({
+    id: uuid(),
+    name,
+    email,
+  })
 })
 
 export default studentsRouter
