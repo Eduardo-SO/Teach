99faf3c7805b0d25827cc0a0419b05756582269b
Eduardo SO <eduardo.sool07@gmail.com>
2020-11-20 00:51:40 -0300

    Add CreateStudents migration

---
+++ server/src/database/migrations/1605842199769-CreateStudents.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class CreateStudents1605842199769 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'students',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('students')
+  }
+}
