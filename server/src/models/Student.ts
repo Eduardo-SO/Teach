e5c77cf81ae48634e73d1dbbf7f07d7594518b2b
Eduardo SO <eduardo.sool07@gmail.com>
2020-11-21 20:51:55 -0300

    Add created_at and updated_at columns to Student model

--- server/src/models/Student.ts
+++ server/src/models/Student.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm'
 
 @Entity('students')
 class Student {
@@ -10,6 +16,12 @@ class Student {
 
   @Column()
   email: string
+
+  @Column(CreateDateColumn)
+  created_at: Date
+
+  @Column(UpdateDateColumn)
+  updated_at: Date
 }
 
 export default Student
