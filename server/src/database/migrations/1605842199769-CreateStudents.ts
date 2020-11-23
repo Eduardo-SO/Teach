26769a7a2541edb33d8e944cc477dbf1e61daab4
Eduardo SO <eduardo.sool07@gmail.com>
2020-11-22 22:49:42 -0300

    Add isUnique to email column

--- server/src/database/migrations/1605842199769-CreateStudents.ts
+++ server/src/database/migrations/1605842199769-CreateStudents.ts
@@ -20,6 +20,7 @@ export default class CreateStudents1605842199769 implements MigrationInterface {
           {
             name: 'email',
             type: 'varchar',
+            isUnique: true,
           },
           {
             name: 'created_at',
