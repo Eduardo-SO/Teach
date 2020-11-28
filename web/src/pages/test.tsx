ac2e10ddf82739b4cda4f951c5ae92508f8cb8b8
Eduardo SO <eduardo.sool07@gmail.com>
2020-11-28 12:30:35 -0300

    Get test questions

--- web/src/pages/test.tsx
+++ web/src/pages/test.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
+
+import api from '../services/api'
+import { useTest } from '../context/test'
 
 import Header from '../components/Header'
 
@@ -12,11 +15,37 @@ import {
 } from '../styles/pages/Quiz'
 import Checkbox from '../components/Checkbox'
 
+interface Question {
+  id: string
+  question: string
+  answers: string
+  correct_answer: string
+}
+
 interface Props {
   toggleTheme(): void
 }
 
 const Home: React.FC<Props> = ({ toggleTheme }) => {
+  const [questions, setQuestions] = useState<Question[]>()
+
+  const { test } = useTest()
+  useEffect(() => {
+    async function loadQuestions() {
+      const loadedQuestions = await api.post('/tests/questions', {
+        questions_id: test.questions
+      })
+
+      console.log(loadedQuestions.data)
+
+      setQuestions(loadedQuestions.data)
+    }
+
+    loadQuestions()
+
+    console.log(questions)
+  }, [])
+
   return (
     <>
       <Head>
@@ -33,6 +62,10 @@ const Home: React.FC<Props> = ({ toggleTheme }) => {
                 Chicago, __________________, is in the Midwest, specifically in
                 the state of Illinois.
               </p>
+
+              {/* {questions.map(question => (
+                <p key={question.id}>{question.question}</p>
+              ))} */}
 
               <Answer>
                 <Checkbox />
